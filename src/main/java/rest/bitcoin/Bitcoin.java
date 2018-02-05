0893876940169658a8a868b9ff6032fa057f404b
milan <milan@TREBLELAPTOP>
2018-02-05 17:55:22 +0100

    changed to getTransaction to iterate starting from the last mined block.

--- src/main/java/rest/bitcoin/Bitcoin.java
+++ src/main/java/rest/bitcoin/Bitcoin.java
@@ -38,7 +38,8 @@ public class Bitcoin {
 	// real keys/address' come from the DB in a later stage
 	final static String clientPrivateKeys1[] = { "92CqrxbHxU1nDQo2N9UkL6bGftcfbh929cYzPSubPshyERqhUK7" };
 	final static String clientPublicKeys1[] = { "03A48BED6D0C1FF608CFBC4F27D7831061A58C927055D0D74B3AD7351E3523D697" };
-	final static String clientAddresses1[] = { "mofhdVSgsUsVacWsf8QMNhDQqYnVXPtnZH", "miQxg3AVaLxZtGEgTbk4YgqU2hu5PoEFDj", "miSnWcTZteByQyJuz4XhVswhHuA5LcFD1i" };
+	final static String clientAddresses1[] = { "mofhdVSgsUsVacWsf8QMNhDQqYnVXPtnZH",
+			"miQxg3AVaLxZtGEgTbk4YgqU2hu5PoEFDj", "miSnWcTZteByQyJuz4XhVswhHuA5LcFD1i" };
 	final static String clientPrivateKeys3[] = { "KySuyZ6jnGbGW2Qc9VpCk7F8z2rqfaS5EfTsqLxycczaZSuPxJwp" };
 	final static String clientPublicKeys3[] = { "021AD207A99E408F840C0911BD8BCDDA9C6089B23AC0CFBB62D76961018E59C282" };
 	final static String clientAddresses3[] = { "1FYU384h3Y7quk1bYy9BZzCFdFA7ojiCfc" };
@@ -79,12 +80,15 @@ public class Bitcoin {
 		// go backwards, starting with the last mined block
 		// FIXME return the actual transaction
 		List<RawTransactionInfo> txs = new LinkedList<RawTransactionInfo>();
-		int blocks = client.getBlockCount();
-		int maxBlocks = Math.min(25000, blocks); // max amount of blocks into the past to go through, about half a year
+		int lastBlockNumber = client.getBlockCount();
+		// look half a year in the past. assume 1 block per 10 minutes.
+		int deepestBlockNumber = lastBlockNumber - 6 * 10 * 24 * 183;
+		// TODO get actual user registration date
 		Date userRegistered = new SimpleDateFormat("dd.MM.yyyy").parse("01.01.2018");
-		for (int i = 0; i <= maxBlocks; i++) {
-			Block block = client.getBlock(i);
-			if (userRegistered.compareTo(block.getTime()) <= 0) {
+		for (int blockNumber = lastBlockNumber; blockNumber >= deepestBlockNumber; blockNumber--) {
+			Block block = client.getBlock(blockNumber);
+			Date blockTime = block.getTime();
+			if (userRegistered.compareTo(blockTime) <= 0) {
 				List<Transaction> transactions = block.getTransactions();
 				if (transactions != null) {
 					for (Transaction transaction : transactions) {
@@ -140,7 +144,7 @@ public class Bitcoin {
 	@Produces({ MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON })
 	public List<Transaction> startTransaction(@QueryParam("toAddress") String toAddress,
 			@QueryParam("satoshiAmount") int satoshiAmount, @QueryParam("changeAddress") String changeAddress) {
-		//TODO create new list of transactions to be signed with key 1
+		// TODO create new list of transactions to be signed with key 1
 		List<Transaction> txs = new LinkedList<>();
 		txs.add(new Transaction(TestNet3Params.get()));
 		txs.add(new Transaction(TestNet3Params.get()));
@@ -153,10 +157,9 @@ public class Bitcoin {
 	@Path("executeTransaction")
 	@Produces({ MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON })
 	public String executeTransaction(List<String> signedTX) {
-		//TODO sign transactions with key 2
-		//TODO broadcast tx
+		// TODO sign transactions with key 2
+		// TODO broadcast tx
 		// TODO return transaction id
 		return "34ca17378d8268631c94ca820ed9e88728dd505d72f2b51847ffdf1f3b1be668";
 	}
-
 }
